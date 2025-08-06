d30179d067f1b5cea6513e65341f839c3b88ef2c
theazizltaief <ltaief2022@gmail.com>
2025-08-06 14:37:55 +0100

    version de sauvgarde

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "controllers" # @0.0.2
+pin "fs" # @2.1.0
+pin "path" # @2.1.0
+pin "process" # @2.1.0
